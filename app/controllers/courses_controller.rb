ae3aba03536b39b2d50619f2a266e689a355f865
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 12:36:54 +0000

    Remove unnecesary ressources

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,4 @@
 class CoursesController < ApplicationController
-  load_and_authorize_resource
   before_action :set_course, only: :destroy
 
   def index
--- app/controllers/enrolments_controller.rb
+++ app/controllers/enrolments_controller.rb
@@ -1,5 +1,4 @@
 class EnrolmentsController < ApplicationController
-  load_and_authorize_resource
   before_action :set_enrolment, only: :destroy
 
   def index
