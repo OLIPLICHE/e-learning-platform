08627e0d9a7336ebca1f619a76d6dff405ad0697
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 10:46:55 +0000

    Add actions courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,35 @@
 class CoursesController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_course, only: :destroy
+
   def index
-    courses = Courses.all
-    render json: courses
+    @courses = current_user.courses.all
+  end
+
+  def show
+  end
+
+  def create
+    @course = current_user.courses.new(course_params)
+
+    if @course.save
+      render :show, status: :created, location: @course
+    else
+      render json: @course.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @course.destroy
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :short_description, :description, :language, :level, :picture, :user_id)
   end
 end
