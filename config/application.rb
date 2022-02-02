ee5bbd7899b6500a9cb6b8d3c82c527ec54800a3
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 18:11:28 +0000

    Add new migration

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ELearningPlatform
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_storage.draw_routes = false
+
 
     # Configuration for the application, engines, and railties goes here.
     #
