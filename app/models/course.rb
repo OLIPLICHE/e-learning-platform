98876f10ab0da8093a6d7110c04124513b85ac55
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 12:52:24 +0000

    Add validation course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   validates :title, length: { maximum: 50 }
+  validates :title, :short_description, :description, :language, :level, :price, :date_start, :date_end, :country, :city, :picture, presence: true
 
   belongs_to :user
   has_many :enrolments, dependent: :destroy
