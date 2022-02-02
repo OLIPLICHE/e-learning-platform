33bc5827b921b754c14b52113a7bb3aee3db499c
OLIPLICHE <olipliche@gmail.com>
2022-02-02 09:47:39 +0100

    Fix linters

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
-    def index
-        courses = Courses.all
-        render json: courses
-    end
+  def index
+    courses = Courses.all
+    render json: courses
+  end
 end
