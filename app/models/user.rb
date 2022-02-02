8998c7ee7850aa1c090ff4b4b7a002b11f1e7cd7
OLIPLICHE <olipliche@gmail.com>
2022-02-02 15:45:11 +0100

    Add authorization 'jwt'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable,
 
   validates :name, presence: true, length: { maximum: 100 }
 
