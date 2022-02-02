ea39495f7403f0fa8c59e9ba098fa719a27b59ff
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 18:19:52 +0000

    Add changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }, path: '',
+  devise_for :users, defaults: { format: :json }, path: 'users',
                      path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }
 
   defaults format: :json do
-    resources :courses, only: %i[create index show update destroy]
-    resources :enrolments, only: %i[create index show update destroy]
+    resources :rooms, only: %i[create index show update destroy]
+    resources :reservations, only: %i[create index show update destroy]
   end
 end
