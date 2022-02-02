ffa5baccf9a4e1fbbb669d7708702861520087ff
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 18:00:34 +0000

    fix devise auth errors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
   end
