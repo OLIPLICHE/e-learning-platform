41abcfd04fb471df1b292ec70fe9c6cbb431a44f
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 12:42:54 +0000

    Fix relationship

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,5 @@ class Course < ApplicationRecord
   validates :title, length: { maximum: 50 }
 
   belongs_to :user
-  belongs_to :enrolment
-  has_one_attached :picture
+  has_many :enrolments, dependent: :destroy
 end
