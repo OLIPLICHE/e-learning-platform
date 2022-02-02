834513ccdfb89813a4e37a49be6e7b0411e3c2b6
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 10:38:05 +0000

    Add method to enrolment controller

--- app/controllers/enrolments_controller.rb
+++ app/controllers/enrolments_controller.rb
@@ -1,2 +1,34 @@
 class EnrolmentsController < ApplicationController
+    load_and_authorize_resource
+  before_action :set_enrolment, only: :destroy
+
+  def index
+    @enrolments = current_user.enrolments.includes(:course)
+  end
+
+  def show; end
+
+  def create
+    @enrolment = current_user.enrolments.new(enrolment_params)
+
+    if @enrolment.save
+      render :show, status: :created, location: @enrolment
+    else
+      render json: @enrolment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @enrolment.destroy
+  end
+
+  private
+
+  def set_enrolment
+    @enrolment = Enrolment.find(params[:id])
+  end
+
+  def enrolment_params
+    params.require(:enrolment).permit(:course_id)
+  end
 end
