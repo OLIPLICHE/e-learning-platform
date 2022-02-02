0192e72a00f808ab945249ef09b4c563f908a026
OLIPLICHE <olipliche@gmail.com>
2022-02-02 15:50:26 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable,
-         :jwt_authenticatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: true, length: { maximum: 100 }
 
