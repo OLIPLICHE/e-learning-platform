b2c4eb48a12e878cb3be32bfc6fe75e0deefe589
codecaiine <akabrouyannickn@gmail.com>
2022-02-02 13:18:55 +0000

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 codecaiine = User.create!(name: 'Codecaiine', email: 'codecaiine@gmail.com', password: '12345678')
 
-course1 = Course.create!(title: 'course1', short_description: 'blababababa', description: 'Bbababa bababab bababababa babababa bababa', price: '100',  picture: 'http://www.pselaw.com/wp-content/uploads/2016/08/pokemon-150x150.jpg', user_id: codecaiine.id)
+course1 = Course.create!(title: 'course1', short_description: 'blababababa', description: 'Bbababa bababab bababababa babababa bababa', price: '100', language: 'french', date_start: '2022-02-1', date_end: '2022-02-9', level: 'Beginner', country: 'Nigeria', city:'Yamoussoukro', picture: 'http://www.pselaw.com/wp-content/uploads/2016/08/pokemon-150x150.jpg', user_id: codecaiine.id)
 
 enrolment = Enrolment.create!(raiting: '5', review: 'Bbababababa', course_id: course1.id, user_id: codecaiine.id)
