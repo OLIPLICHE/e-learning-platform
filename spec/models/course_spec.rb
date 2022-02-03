699c784b0248c4acb567691ee008b48beaa9e6f9
OLIPLICHE <olipliche@gmail.com>
2022-02-03 17:34:36 +0100

    Prepare testing for courses

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(title: 'Anything', description: 'Anything', short_description: 'Anything',
+                        city: 'Anything', picture: 'Anything', presence: 'true', length: 'anything')
+  end
+
+  it 'is not valid without a title'
+  it 'is not valid without a description'
+  it 'is not valid without a short_description'
+  it 'is not valid without a length'
+  it 'is not valid without a city'
+  it 'is not valid without a picture'
+  it 'is not valid without a presence'
 end
--- spec/requests/enrolments_spec.rb
+++ spec/requests/enrolments_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Enrolments", type: :request do
-  describe "GET /enrolments" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Enrolments', type: :request do
+  describe 'GET /enrolments' do
+    it 'works! (now write some real specs)' do
       get enrolments_path
       expect(response).to have_http_status(200)
     end
