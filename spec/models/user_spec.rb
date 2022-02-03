5cc38b8ae0c8d1c382c0652f0a705975d3636231
codecaiine <akabrouyannickn@gmail.com>
2022-02-03 22:35:37 +0000

    Add user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'Paka', email: 'example@mail.com', password: 'mypassword')
+  end
+  before { subject.save }
+  it 'name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the name' do
+    subject.name = 'Paka'
+    expect(subject).to be_valid
+  end
+
+  it 'should not be greater than 100' do
+    subject.name = 'Paka'*100
+    expect(subject).to_not be_valid
+  end
 end
