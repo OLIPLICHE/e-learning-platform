b559052e126f5b84eb71413617ec8e89c74ea0c2
codecaiine <akabrouyannickn@gmail.com>
2022-02-03 23:09:32 +0000

    Fix linters errors

--- spec/models/enrolment_spec.rb
+++ spec/models/enrolment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Enrolment, type: :model do
   let(:user) { User.create(name: 'Yannick', email: 'example@mail.com', password: 'mypassword') }
   let(:course) do
     Course.create(user_id: user.id, title: 'Ruby on Rails module', picture: 'logo.png', city: 'Yakro', country: 'France', short_description: 'lala lallal', description: 'Zoz zoz ozoz', language: 'french', level: 'Beginner',
-    price: '100', date_start: '2015-01-9', date_end: '2015-01-7')
+                  price: '100', date_start: '2015-01-9', date_end: '2015-01-7')
   end
   let(:enrolment) do
     Enrolment.create(rating: '5', review: 'rrr bbbb', course_id: course.id, user_id: user.id)
@@ -40,3 +40,4 @@ RSpec.describe Enrolment, type: :model do
       expect(enrolment).to be_valid
     end
   end
+end
