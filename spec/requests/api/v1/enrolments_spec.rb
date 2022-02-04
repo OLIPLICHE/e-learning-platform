04ebe429f943ab3364e2e567aa76efd46b52463f
codecaiine <akabrouyannickn@gmail.com>
2022-02-04 19:50:20 +0000

    Add spec to get all enrolments

--- spec/requests/api/v1/enrolments_spec.rb
+++ spec/requests/api/v1/enrolments_spec.rb
@@ -1,12 +1,12 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/enrolments', type: :request do
-
   path '/api/v1/enrolments' do
-
     get('list enrolments') do
-      response(200, 'successful') do
+      tags 'Enrolments'
+      security [bearer_auth: []]
 
+      response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -16,53 +16,12 @@ RSpec.describe 'api/v1/enrolments', type: :request do
         end
         run_test!
       end
-    end
-
-    post('create enrolment') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response '201', 'Authorized' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('admin@admin.com:2435647')}" }
         run_test!
       end
-    end
-  end
-
-  path '/api/v1/enrolments/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('show enrolment') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('delete enrolment') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response '401', 'authentication failed' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
         run_test!
       end
     end
