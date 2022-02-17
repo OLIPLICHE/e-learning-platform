e15430f127948cd2abc65ec1f51a035bf630f489
codecaiine <akabrouyannickn@gmail.com>
2022-02-17 14:45:48 +0000

    Delete corrupt file

--- app/models/course.rb
+++
@@ -1,9 +0,0 @@
-class Course < ApplicationRecord
-  validates :title, :city, length: { maximum: 50 }
-  validates :title, :short_description, :description, :language, :level, :price, :date_start, :date_end, :country,
-            :city, :picture, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-
-  belongs_to :user
-  has_many :enrolments, dependent: :destroy
-end
